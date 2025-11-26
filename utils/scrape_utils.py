21e8dfe6c79164ff05dbd95d636abed0b4914124
Cavv <cavalierematteo2004@gmail.com>
2025-11-26 12:26:33 +0100

    Add separate "create_scraper_session" function in scrape utils

--- utils/scrape_utils.py
+++ utils/scrape_utils.py
@@ -2,14 +2,29 @@
 This module provides utilities for scraping web content and caching responses.
 """
 import cloudscraper
+
 from utils import cache_manager
+
+CURL_HEADERS = {
+    'User-Agent': 'curl/8.13.0',
+    'Accept': '*/*'
+}
+
+
+def create_scraper_session(headers=None):
+    """Create a scraper session and optionally apply custom headers."""
+    session = cloudscraper.create_scraper()
+    applied_headers = headers or CURL_HEADERS
+    if applied_headers:
+        session.headers.update(applied_headers)
+    return session
 
 
 def fetch_url(url, session=None):
     """Fetch the content of a URL and cache the response."""
     if not session:
         # Create a new session if none is provided
-        session = cloudscraper.create_scraper()
+        session = create_scraper_session(CURL_HEADERS)
 
     # Perform the GET request with no timeout specified
     r = session.get(url, timeout=None)
